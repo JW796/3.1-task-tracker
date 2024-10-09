810c3863c954264d9d7f8d21d745fb6881b66a47
James Joseph <98562132+JW796@users.noreply.github.com>
2024-10-09 18:49:05 -0400

    Add Task

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import AddTask from "./components/AddTask";
+
 function App() {
   return (
     <>
-    <h1>03 - Task Tracker</h1>
+      <h1 className="text-2xl font-bold py-4 pl-6">03 - Task Tracker</h1>
+      <p className="text-xl pl-6">Hello</p>
+      <div className="flex flex-row items-center">
+        <p className="text-xl pl-6">Click</p>
+        <AddTask />
+        <p className="text-xl my-2">to add a new task</p>
+      </div>
     </>
   );
 }
