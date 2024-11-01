5af17a07a428f53092a2951c304f750ab169ecce
James Joseph <98562132+JW796@users.noreply.github.com>
2024-11-01 12:40:17 -0400

    WIP 3 Timer render + Styling

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -3,13 +3,15 @@ import EditTask from "./EditTask";
 
 const ToDo = ({ task, index, taskList, setTaskList }) => {
   const [time, setTime] = useState(0);
+  const [running, setRunning] = useState(false);
 
-  const handleDelete = itemID => {
+  const handleDelete = (itemID) => {
     let removeIndex = taskList.indexOf(task);
     taskList.splice(removeIndex, 1);
-    setTaskList((currentTasks =>
-      currentTasks.filter(todo => todo.id !== itemID)))
-  }
+    setTaskList((currentTasks) =>
+      currentTasks.filter((todo) => todo.id !== itemID)
+    );
+  };
 
   return (
     <>
@@ -24,15 +26,20 @@ const ToDo = ({ task, index, taskList, setTaskList }) => {
           />
         </div>
         <p className="text-lg py-2">{task.taskDescription}</p>
-        <div>
-          <div>
+        <div className="w-full flex flex-col items-center justify-center">
+          <div className="text-xl font-semibold py-4">
             <span>{("0" + Math.floor((time / 3600000) % 24)).slice(-2)}:</span>
             <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
             <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}</span>
             <span>{("0" + Math.floor((time / 10) % 100)).slice(-2)}</span>
           </div>
-          <div>
-
+          <div className="w-1/3 max-w-sm flex flex-row justify-evenly gap-2">
+            {running ? (
+              <button className="border rounded-lg py-1 px-3">Stop</button>
+            ) : (
+              <button className="border rounded-lg py-1 px-3">Start</button>
+            )}
+            <button className="border rounded-lg py-1 px-3">Reset</button>
           </div>
         </div>
         <div className="w-full flex justify-center">
