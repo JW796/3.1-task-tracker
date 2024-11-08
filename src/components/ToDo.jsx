26bf560e8feb200f59b93338554031a2516f78d0
James Joseph <98562132+JW796@users.noreply.github.com>
2024-11-08 13:59:26 -0500

    WIP 2 Local storage

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import EditTask from "./EditTask";
 
 const ToDo = ({ task, index, taskList, setTaskList }) => {
-  const [time, setTime] = useState(0);
+  const [time, setTime] = useState(task.duration);
   const [running, setRunning] = useState(false);
 
   useEffect(() => {
@@ -18,12 +18,29 @@ const ToDo = ({ task, index, taskList, setTaskList }) => {
     return () => clearInterval(interval);
   }, [running]);
 
-  const handleDelete = (itemID) => {
+  const handleStop = () => {
+    setRunning(false);
+
+    let taskIndex = taskList.indexOf(task);
+    taskList.splice(taskIndex, 1, {
+      projectName: task.projectName,
+      taskDescription: task.taskDescription,
+      timestamp: task.timestamp,
+      duration: time
+    })
+
+    localStorage.setItem("taskList", JSON.stringify(taskList))
+    window.location.reload();
+  }
+
+  const handleDelete = itemID => {
     let removeIndex = taskList.indexOf(task);
     taskList.splice(removeIndex, 1);
-    setTaskList((currentTasks) =>
-      currentTasks.filter((todo) => todo.id !== itemID)
-    );
+    localStorage.setItem("taskList",  JSON.stringify(taskList));
+    window.location.reload();
+    // setTaskList((currentTasks) =>
+    //   currentTasks.filter((todo) => todo.id !== itemID)
+    // );
   };
 
   return (
@@ -50,9 +67,7 @@ const ToDo = ({ task, index, taskList, setTaskList }) => {
             {running ? (
               <button
                 className="border rounded-lg py-1 px-3"
-                onClick={() => {
-                  setRunning(false);
-                }}
+                onClick={handleStop}
               >
                 Stop
               </button>
